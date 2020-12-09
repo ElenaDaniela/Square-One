d508c11b03710442ba15b2408bfbfddda69e7264
Dana <budadaniela68@yahoo.com>
2020-12-09 20:54:48 +0200

    Jump si movement
    - De reparat box collider-ul

---
+++ Square One/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Collider2D coll;
+    private float moveInput;
+    private bool isGrounded;
+    private float jumpTimeCounter;
+    private bool isJumping;
+    
+    public float speed;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    public float jumpForce;
+    public float jumpTimer;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
+    }
+    private void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        if (moveInput < 0)
+        {
+            transform.localScale = new Vector2(-1,1);
+        }
+        if(moveInput > 0)
+        {
+            transform.localScale = new Vector2(1,1);
+        }
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            //isJumping = true;
+            //jumpTimeCounter = jumpTimer;
+            rb.velocity = Vector2.up * jumpForce; 
+        }
+        
+        /*if (Input.GetKey(KeyCode.Space) && isJumping == true)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                isJumping = false;
+                rb.velocity = Vector2.down * jumpForce;
+            }
+             
+        }*/
+    }
+}
