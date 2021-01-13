c76dbfbc0a8364dd089c795b5b8b11097adc9ac7
Dana <budadaniela68@yahoo.com>
2021-01-13 17:30:41 +0200

    Mici modificari

--- Square One/Assets/Scripts/PlayerController.cs
+++ Square One/Assets/Scripts/PlayerController.cs
@@ -19,6 +19,9 @@ public class PlayerController : MonoBehaviour
     public LayerMask whatIsGround;
     public float jumpForce;
     public float jumpTimer;
+    
+    //public Transform height;
+    //bool prev_grounded = false;
 
     private void Start()
     {
@@ -27,6 +30,14 @@ public class PlayerController : MonoBehaviour
     }
     private void Update()
     {
+       /* if (isGrounded1 == true || isGrounded2 == true)
+        {
+            prev_grounded = true;
+        }
+        else if (isGrounded1 == false && isGrounded2 == false)
+        {
+            prev_grounded = false;
+        }*/
         moveInput = Input.GetAxisRaw("Horizontal");
         
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
@@ -38,12 +49,18 @@ public class PlayerController : MonoBehaviour
         {
             transform.localScale = new Vector2(1,1);
         }
-        
+
+        /*if ((isGrounded1 == true || isGrounded2 == true) && prev_grounded == false)
+        {
+            height.GetComponent<Animator>().SetTrigger("squash");
+            Debug.Log("merge");
+        }*/
         isGrounded1 = Physics2D.OverlapCircle(feetPos1.position, checkRadius, whatIsGround);
         isGrounded2 = Physics2D.OverlapCircle(feetPos2.position, checkRadius, whatIsGround);
         if ((isGrounded1 == true || isGrounded2 == true) && Input.GetKeyDown(KeyCode.Space))
         {
             rb.velocity = Vector2.up * jumpForce; 
+           // height.GetComponent<Animator>().SetTrigger("stretch");
         }
     }
 }
