b88f1b23fa0b05a15a1ba35fabf3e903481d1203
Dana <budadaniela68@yahoo.com>
2021-03-04 18:17:22 +0200

    Terminarea animatiilor caractere si switch intre caractere (mouse)

---
+++ Square One/Assets/Scripts/MindScript.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MindScript : MonoBehaviour
+{
+    public GameObject[] Players;
+    [SerializeField] GameObject CurrentPlayer;
+
+    private void Start()
+    {
+        for (int i = 0; i <= Players.Length; i++)
+        {
+            if(Players[i] != CurrentPlayer)
+                Players[i].GetComponent<PlayerController>().enabled = false;
+        }
+    }
+
+    public void changePlayer(GameObject player)
+    {
+        CurrentPlayer.GetComponent<PlayerController>().enabled = false;
+        CurrentPlayer = player;
+    }
+}
---
+++ Square One/Assets/Scripts/PlayerSwitch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSwitch : MonoBehaviour
+{
+   public MindScript mind; 
+
+   void OnMouseDown()
+   {
+      mind.changePlayer(this.gameObject);
+      GetComponent<PlayerController>().enabled = true;
+   }
+}
