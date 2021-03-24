a2cad3c896889cde1984b66815e6e6a6f18f50bb
Dana <budadaniela68@yahoo.com>
2021-03-24 16:31:31 +0200

    Adaugat double jump doar pe un caracter

--- Square One/Assets/Scripts/PlayerController.cs
+++ Square One/Assets/Scripts/PlayerController.cs
@@ -9,30 +9,41 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private Collider2D coll;
     private float moveInput;
-    private bool isGrounded1, isGrounded2;
-    private float jumpTimeCounter;
-    private bool isJumping;
+    private bool isGrounded1, isGrounded2, isGrounded;
+    private int jumpCount = 0;
+    private float jumpCooldown;
     
     public float speed;
     public Transform feetPos1, feetPos2;
     public float checkRadius;
-    public LayerMask whatIsGround;
+    [SerializeField] LayerMask whatIsGround;
     public float jumpForce;
-    public float jumpTimer;
-
+    [SerializeField] int extraJumps = 0;
     public Animator animator;
+
+    
     
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        Debug.Log("test");
     }
     private void Update()
     {
-       
         moveInput = Input.GetAxisRaw("Horizontal");
         
+        animator.SetFloat("Speed", Mathf.Abs(moveInput*speed));
+        
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+        CheckGround();
+    }
+
+    private void FixedUpdate()
+    {
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
         if (moveInput < 0)
         {
@@ -42,13 +53,34 @@ public class PlayerController : MonoBehaviour
         {
             transform.localScale = new Vector2(1,1);
         }
+    }
 
-        animator.SetFloat("Speed", Mathf.Abs(moveInput*speed));
+    void Jump()
+    {
+        if (isGrounded || jumpCount < extraJumps)
+        {
+            Debug.Log(isGrounded);
+            Debug.Log(jumpCount < extraJumps);
+            rb.velocity = Vector2.up * jumpForce;
+            jumpCount++;
+        }
+    }
+    void CheckGround()
+    {
         isGrounded1 = Physics2D.OverlapCircle(feetPos1.position, checkRadius, whatIsGround);
         isGrounded2 = Physics2D.OverlapCircle(feetPos2.position, checkRadius, whatIsGround);
-        if ((isGrounded1 == true || isGrounded2 == true) && Input.GetKeyDown(KeyCode.Space))
+        if (isGrounded1 == true || isGrounded2 == true)
         {
-            rb.velocity = Vector2.up * jumpForce; 
+            isGrounded = true;
+            jumpCount = 0;
+            jumpCooldown = Time.time + 0.2f;
+        }else if (Time.time < jumpCooldown && extraJumps > 0)
+        {
+            isGrounded = true;
         }
-    } 
+        else
+        {
+            isGrounded = false;
+        }
+    }
 }
